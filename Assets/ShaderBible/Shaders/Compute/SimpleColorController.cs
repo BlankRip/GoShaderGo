b31f5903d86780b03d2abab09797b77a4fbb39d3
BlankRip <aniketh_g5@outlook.com>
2022-03-21 15:52:06 +0530

    Started Compute Shader section

---
+++ Assets/ShaderBible/Shaders/Compute/SimpleColorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleColorController : MonoBehaviour
+{
+    [SerializeField] ComputeShader cShader;
+    [SerializeField] Texture tex;
+    [SerializeField] RenderTexture mainTex;
+    [SerializeField] int texSize = 256;
+    private Renderer renderer;
+
+    private void Start() {
+        mainTex = new RenderTexture(texSize, texSize, 0, RenderTextureFormat.ARGB32);
+        mainTex.enableRandomWrite = true;
+        mainTex.Create();
+        
+        renderer = GetComponent<Renderer>();
+        renderer.enabled = true;
+
+        cShader.SetTexture(0, "Result", mainTex);
+        cShader.SetTexture(0, "ColTex", tex);
+        renderer.material.SetTexture("_MainTex", mainTex);
+
+        cShader.Dispatch(0, texSize/8, texSize/8, 1);
+    }
+}
